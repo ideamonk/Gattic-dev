e9868bfe7dfbeb7b35d418aeca0aa0d4d3780fe1
Abhishek Mishra <ideamonk@gmail.com>
2010-09-29 20:56:45 +0530

    started an XCode project, glitches to be dealt later, SWOT done

---
+++ chatmasala/build/Debug/chatmasala.app/Contents/Resources/chatmasalaAppDelegate.py
@@ -0,0 +1,14 @@
+#
+#  chatmasalaAppDelegate.py
+#  chatmasala
+#
+#  Created by ideamonk on 29/09/10.
+#  Copyright ideamonk.in 2010. All rights reserved.
+#
+
+from Foundation import *
+from AppKit import *
+
+class chatmasalaAppDelegate(NSObject):
+    def applicationDidFinishLaunching_(self, sender):
+        NSLog("Application did finish launching.")
---
+++ chatmasala/main.py
@@ -0,0 +1,20 @@
+#
+#  main.py
+#  chatmasala
+#
+#  Created by ideamonk on 29/09/10.
+#  Copyright ideamonk.in 2010. All rights reserved.
+#
+
+#import modules required by application
+import objc
+import Foundation
+import AppKit
+
+from PyObjCTools import AppHelper
+
+# import modules containing classes required to start application and load MainMenu.nib
+import chatmasalaAppDelegate
+
+# pass control to AppKit
+AppHelper.runEventLoop()
